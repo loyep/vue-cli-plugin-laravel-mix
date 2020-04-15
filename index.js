7d913a6968b4f439022c25f03298387db767ec95
Zack <mwl@live.com>
2020-04-15 18:08:13 +0800

    更新

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-const modifyConfig = (config, fn) => {
-  if (Array.isArray(config)) {
-    config.forEach(c => fn(c))
-  } else {
-    fn(config)
-  }
-}
-
 module.exports = (api, options) => {
   const defaultOptions = {
     resourcePath: api.resolve('./resources/assets'),
     entry: api.resolve('resources/assets/app.js')
   }
 
-  const pluginOptions = Object.assign({}, defaultOptions, (options.pluginOptions ? options.pluginOptions.laravel || {} : {}));
+  const pluginOptions = Object.assign({}, defaultOptions, (options.pluginOptions ? options.pluginOptions.laravel || {} : {}))
   const { build } = api.service.commands
 
   api.registerCommand('watch', {
-      description: 'watch for production',
-      usage: 'vue-cli-service watch',
-      options: {}
+    description: 'watch for production',
+    usage: 'vue-cli-service watch',
+    options: {}
   }, async (args) => {
-      args.watch = true
-      process.env.VUE_APP_WATCH_MODE = 'true';
-      await build.fn(args).then(() => {
-        // delete process.env.VUE_APP_WATCH_MODE
-      });
+    args.watch = true
+    process.env.VUE_APP_WATCH_MODE = 'true'
+    await build.fn(args).then(() => {
+      // delete process.env.VUE_APP_WATCH_MODE
+    })
   })
 
   api.registerCommand('analyzer', {
@@ -32,7 +24,7 @@ module.exports = (api, options) => {
     usage: 'vue-cli-service analyzer',
     options: {}
   }, async (args) => {
-    process.env.VUE_APP_ANALYZER_MODE = 'true';
+    process.env.VUE_APP_ANALYZER_MODE = 'true'
     await build.fn(args).then(() => {
 
     })
@@ -41,21 +33,19 @@ module.exports = (api, options) => {
   api.chainWebpack(webpackConfig => {
     const target = process.env.VUE_CLI_BUILD_TARGET
     if (target && target !== 'app') {
-        return
+      return
     }
 
-    webpackConfig
-      .entry('app')
-        .delete('./src/main.js')
-        .add(pluginOptions.entry)
-        .end()
-      .resolve
-        .alias
-        .set('@', pluginOptions.resourcePath)
-        .end()
-    
+    webpackConfig.entry('app')
+      .delete('./src/main.js')
+      .add(pluginOptions.entry)
+
+    webpackConfig.resolve
+      .alias
+      .set('@', pluginOptions.resourcePath)
+
     if (process.env.VUE_APP_WATCH_MODE) {
-      webpackConfig.optimization.minimize(false) 
+      webpackConfig.optimization.minimize(false)
     }
 
     if (process.env.VUE_APP_ANALYZER_MODE) {
@@ -66,17 +56,16 @@ module.exports = (api, options) => {
     }
 
     if (process.env.NODE_ENV === 'production') {
-      const manifestPath = api.resolve('public/static/mix-manifest.json')
+      const manifestPath = pluginOptions.manifestPath || api.resolve(`${options.outputDir}/mix-manifest.json`)
       webpackConfig
         .plugin('laravel')
-          .use(require('webpack-manifest-plugin'), [{
-            fileName: manifestPath,
-            publicPath: '/',
-            basePath: '/',
-            filter: desc => (desc.path.endsWith('.js') || desc.path.endsWith('.css')) && !desc.path.includes('precache-manifest')
-          }])
+        .use(require('webpack-manifest-plugin'), [{
+          fileName: manifestPath,
+          publicPath: '/',
+          basePath: '/',
+          filter: desc => (desc.path.endsWith('.js') || desc.path.endsWith('.css')) && !desc.path.includes('precache-manifest')
+        }])
     }
-
   })
 
   api.configureDevServer(app => {
